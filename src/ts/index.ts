c28b55d114eea3dbf6f66a090ca2b85cc21a568c
Allan <allan.queiroz.dev@gmail.com>
2023-09-12 00:06:18 -0300

    utilizando typescript

---
+++ src/ts/index.ts
@@ -0,0 +1,15 @@
+window.onload = function () {
+  console.log("Todos os recursos terminaram o carregamento!");
+};
+
+const arrayNumeros = [5, 51, 2, 26, 6, 1, 20];
+const arrFormatado = arrayNumeros.sort();
+const arrFormatadoCorretamente = arrayNumeros.sort((a, b) => a - b);
+
+const arr1 = document.querySelector('#arr-1');
+if(arr1) arr1.innerHTML = 
+`
+Original: [${arrayNumeros}] \n
+Formatado-errado: [${arrFormatado}]
+Formatado-certo: [${arrFormatadoCorretamente}]
+`;
